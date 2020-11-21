49c807b66201da41f33f62c39824cd43480142d8
Hayk Abrahamyan <abrahamyan.hayk.03@gmail.com>
2020-11-21 07:40:27 +0400

    Setup sockets

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv'
-import app from './app'
 import http from 'http'
+
+
+import app from './app'
+import { initSocket } from './socket'
 
 dotenv.config()
 
 const PORT = process.env.PORT
 
 const server = http.createServer(app)
+initSocket(server)
 
 server.listen(PORT)
